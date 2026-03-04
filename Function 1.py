52d24f090cf91f1d42a1953a49eee14def31f0a8
samdganta <41996898+samdganta@users.noreply.github.com>
2026-03-04 18:40:55 +0000

    Add files via upload

---
+++ Function 1.py
@@ -0,0 +1,43 @@
+def intro(name):
+
+ print("Hello, Good morning! I am", name)
+
+user_name = input("PAUL")
+intro(user_name)
+
+# Factorial of a number using recursion\
+def recur_factorial(n):
+ if n == 1:
+  return n 
+ else:
+  return n*recur_factorial(n-1)
+ 
+num= int(input(""))
+
+ #check if the number is nagative
+if num < 0:
+  print("Sorry factorial does not exist for negative numbers")
+elif num== 0:
+ print('The factorial of 0 is 1')
+else:
+ print("the factorial of ", num, "is", recur_factorial (num))
+
+def add(x,y):
+ return x + y
+
+def subtract(x,y):
+ return x - y
+
+def multiply(x,y):
+ return x * y
+
+def divide(x,y):
+ return x / y 
+
+num1 = int(input("Enter Number 1"))
+num2 = int(input("Enter number 2"))
+
+print("Sum :", add(num1, num2))
+print("Difference :", subtract(num1, num2))
+print("Product :", multiply(num1, num2))
+print("Quotient :", divide(num1, num2))
